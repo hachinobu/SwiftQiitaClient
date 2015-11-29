9a52cac71d796ec369f8c29a159a4e5c253a02b2
hachinobu <hachinobu@gmail.com>
2015-11-29 18:09:17 +0900

    first commit

---
+++ Podfile
@@ -0,0 +1,10 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '8.0'
+use_frameworks!
+
+pod 'Alamofire', '~> 3.0'
+pod 'ObjectMapper', '~> 1.0'
+pod 'ReactKit'
+pod 'Bond', '~> 4.0'
+pod 'R.swift'
+pod 'Timepiece'
---
+++ SwiftQiitaClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftQiitaClient
+//
+//  Created by Takahiro Nishinobu on 2015/11/29.
+//  Copyright © 2015年 hachinobu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
